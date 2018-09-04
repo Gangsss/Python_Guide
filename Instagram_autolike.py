8293379f3ffc673b6df580ff703ce17a8b5864ef
Gangsss <42054789+Gangsss@users.noreply.github.com>
2018-09-04 22:13:22 +0900

    위치로 검색하기

--- Instagram_autolike.py
+++ Instagram_autolike.py
@@ -3,6 +3,8 @@ import random
 from selenium import webdriver
 from urllib.parse import quote
 from selenium.common.exceptions import NoSuchElementException
+import request
+from bs4 import BeautifulSoup
 
 
 id = 'id'
@@ -11,6 +13,7 @@ password = 'password'
 timeline_like_count = 80
 hash_tags = ['안암','건대','보아즈']
 hash_tags_count = 120
+location = ['서울, 서울역, 건대입구역']
 
 browser = webdriver.Chrome('C:/Users/Kim/Desktop/chromedriver')
 
@@ -106,10 +109,42 @@ def hash_tags_like(hash_tags, hash_tags_count):
     print('-'*20)
     print('끝났습니다~')
 
+def search_by_loc(location):
+    for i in range(len(location)):
+        browser.get('https://www.instagram.com')
+        time.sleep(3)
+        browser.set_window_size(1024, 600) # 페이지가 어느정도 커야 검색 창이 보이므로 고정 시킨다.
+        time.sleep(2)
+        # 반복시 검색어가 쌓인다. 이를 방지하기 위해 처음에 검색어 삭제버튼을 누르기 
+        browser.find_element_by_css_selector('div.pbgfb.Di7vw ').click()
+        time.sleep(2)
+        browser.find_element_by_css_selector('div.aIYm8.coreSpriteSearchClear').click()
+        time.sleep(2)
+        browser.find_element_by_css_selector("input.XTCLo.x3qfX ").send_keys(location[i])
+        time.sleep(2)
+            
+        html= browser.page_source
+        soup = BeautifulSoup(html,'html.parser')
+        page_all = soup.select('a.yCE8d')
+
+        # 위치는 태그로 검색하는 것과 다르게 #이 붙어 있지않다. 
+        # .text()를 이용해 tag안의 텍스트를 가져오고 그것이 location 이 같으면
+        # href에 있는 주소를 가져다가 쓴다.
+        url =''
+    
+        for page in page_all:
+            if page.text == location:
+                url+= page.get('href')
+                print('검색한 '+location[i]+'이(가) 위치로 등록 되어있습니다.')
+        if url =='':
+            print('검색한 '+ location[i] + '이(가) 위치로 등록이 안되어있습니다.')
+             
+
 
 login(id, password)
 timeline_like(timeline_like_count)
 hash_tags_like(hash_tags, hash_tags_count)
+search_by_location(location)
 
 browser.quit()
 
